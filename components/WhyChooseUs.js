cf5a1ff0210647cb7266a7601e2726f2098e7cc8
InolofatsengMotloba <motlobainolo123@gmail.com>
2026-01-22 13:17:10 +0200

    Remove icons

--- components/WhyChooseUs.js
+++ components/WhyChooseUs.js
@@ -1,33 +1,21 @@
 import React from "react";
-import {
-  FaHardHat,
-  FaHandshake,
-  FaUserTie,
-  FaUsers,
-  FaClipboardCheck,
-} from "react-icons/fa";
 
 const whyChooseUsData = [
   {
     title: "Proven track record on **multi-million-rand** projects",
-    // icon: <FaHardHat size={20} />,
   },
   {
     title:
       "Experience with **mining houses**, municipalities, and **tier-1 contractors**",
-    // icon: <FaHandshake size={20} />,
   },
   {
     title: "Strong **technical leadership** and on-site management",
-    // icon: <FaUserTie size={20} />,
   },
   {
     title: "Commitment to **local empowerment** and job creation",
-    // icon: <FaUsers size={20} />,
   },
   {
     title: "**Reliable delivery** within time and budget",
-    // icon: <FaClipboardCheck size={20} />,
   },
 ];
 
@@ -36,8 +24,6 @@ export default function WhyChooseUs() {
     <section className="bg-[#F1F1F1] py-20 text-[#212471]">
       {/* Container matches Projects section width and padding exactly */}
       <div className="max-w-7xl mx-auto px-4 md:px-5 lg:px-6">
-        
-
         {/* Grid Container */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {/* Header Block with Project-style text hierarchy */}
@@ -48,7 +34,6 @@ export default function WhyChooseUs() {
               Expertise. */}
             </h2>
             <div className="mt-3 w-12 h-0.5 bg-[#212471] rounded-xl"></div>
-           
           </div>
 
           {/* Service Cards */}
@@ -88,7 +73,7 @@ function ServiceBox({ item }) {
       </div>
 
       <div className="mt-4 flex justify-end">
-        <div className="w-6 h-[1px] bg-[#212471]/10 group-hover:w-10 group-hover:bg-[#212471]/40 transition-all"></div>
+        <div className="w-6 h-px bg-[#212471]/10 group-hover:w-10 group-hover:bg-[#212471]/40 transition-all"></div>
       </div>
     </div>
   );
